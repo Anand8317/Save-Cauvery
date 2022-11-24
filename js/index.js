1c520d3776478c4f1176ee4f9c458692c06f924f
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-11-24 16:45:26 +0530

    Fixed js linter errors

--- js/index.js
+++ js/index.js
@@ -6,73 +6,71 @@ const header = document.querySelector('.nav');
 const headerItems = document.getElementsByClassName('toggle1');
 const speaker = document.querySelector('.speaker-container');
 
-
 menuButton.addEventListener('click', () => {
   Array.from(bar).forEach((element) => {
     element.classList.toggle('pressed');
-  })
+  });
   navSocial.classList.toggle('active');
   navBar.classList.toggle('active');
   menuButton.classList.toggle('pressed');
   header.classList.toggle('pressed');
-})
+});
 
 if (bar[0].classList.contains('pressed')) {
-Array.from(headerItems).forEach((element) => {
-  element.addEventListener('click', () => {
-    Array.from(bar).forEach((element) => {
-      element.classList.toggle('pressed');
-    })
-    navSocial.classList.toggle('active');
-    navBar.classList.toggle('active');
-    menuButton.classList.toggle('pressed');
-    header.classList.toggle('pressed');
-  })
-})
+  Array.from(headerItems).forEach((element) => {
+    element.addEventListener('click', () => {
+      Array.from(bar).forEach((element) => {
+        element.classList.toggle('pressed');
+      });
+      navSocial.classList.toggle('active');
+      navBar.classList.toggle('active');
+      menuButton.classList.toggle('pressed');
+      header.classList.toggle('pressed');
+    });
+  });
 }
-
 
 const speakerList = [
   {
     img: '../Images/s1.png',
     name: 'Rajendra Singh',
     des1: 'Indian water conservationist and environmentalist.',
-    des2: 'Also known as "waterman of India", he won the Magsaysay Award in 2001.'
+    des2: 'Also known as "waterman of India", he won the Magsaysay Award in 2001.',
   },
   {
     img: '../Images/s2.png',
     name: 'Amla Ruia',
     des1: 'Amla Ruia, a Mumbai-based social activist',
-    des2: 'she founded Aakar Charitable Trust. Up until now, the Trust has helped in the construction of 2000'
+    des2: 'she founded Aakar Charitable Trust. Up until now, the Trust has helped in the construction of 2000',
   },
   {
     img: '../Images/s3.png',
     name: 'Aabid Surti',
     des1: 'Aabid Surti runs a one-man NGO named the Drop Dead Foundation',
-    des2: 'The 80-year-old does it all for free, with his team of one volunteer and a plumber.'
+    des2: 'The 80-year-old does it all for free, with his team of one volunteer and a plumber.',
   },
   {
     img: '../Images/s4.png',
     name: 'Ayyappa Masagi',
     des1: 'Ayyappa Masagi has changed the lives of thousands',
-    des2: 'He started planting rubber and coffee'
+    des2: 'He started planting rubber and coffee',
   },
   {
     img: '../Images/s5.png',
     name: 'Shirish Apte',
     des1: 'An executive engineer with the Minor Irrigation',
-    des2: 'Recharged groundwater levels and increased agricultural output'
+    des2: 'Recharged groundwater levels and increased agricultural output',
   },
   {
     img: '../Images/s6.png',
     name: 'Chandra Kishore Patil',
     des1: 'Chandra Kishore Patil, a resident of Indiranagar, Nashik',
-    des2: 'Despite him being faced by rude people'
+    des2: 'Despite him being faced by rude people',
   },
 
-]
+];
 
-for( let i=0; i<speakerList.length; i++) {
+for (let i = 0; i < speakerList.length; i += 1) {
   speaker.innerHTML += `
   <div class="speaker">
         <img src="${speakerList[i].img}">
@@ -83,5 +81,5 @@ for( let i=0; i<speakerList.length; i++) {
           <p>${speakerList[i].des2}</p>
       </div>
   </div>
-  `
+  `;
 }
